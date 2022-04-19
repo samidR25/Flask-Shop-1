681dca82f12d7a8c9334b4863d275a331e282db3
Samidur Rahman <rahmans13@cardiff.ac.uk>
2022-04-19 22:43:45 +0100

    Initial commit

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+app = Flask(__name__, static_folder="static")
+
+app.config['SECRET_KEY'] = '<7e5a36d7d8a56464191621dd38c0a35d6da35e3c088a3ce5>'
 
 from shop import routes
--- shop/routes.py
+++ shop/routes.py
@@ -1,5 +1,14 @@
 from shop import app
+from flask import Flask, render_template, url_for, request, redirect, flash, session
 
 @app.route("/")
 def front_page():
-    return "TODO: make a shop!"
+    return render_template('home.html', title='Home')
+
+@app.route("/home")
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About Me')
